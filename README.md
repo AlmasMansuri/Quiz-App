db25f5329340e9e29fb7c12e20a2f38b280f48a8
Almas Mansuri <almas.a.mansuri@gmail.com>
2024-01-15 22:35:32 +0000

    Questions options and InitialEl functionality added

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Quiz-App
-Timed coding quiz with multiple choice questions 
+
+Timed coding quiz with multiple choice questions
+
+# Description :
+
+A Quiz has been made with the following functions:
+
+- When A start quiz button is clicked a timer starts and the first question appears.
+
+- Questions contains four option buttons for each answer.
+- When answer is clicked, the next question appears.
+- When the answer clicked is incorrect then the time is subtracted from the clock.
+- The quiz ends when all questions are answered or the timer reaches 0.
+- When the quiz ends, it should display the score.
+- Also the user can save their initials and score at the end of the quiz.
+
+# The screenshot of working quiz:
+
+# URl of the deployed application
--- assets/js/commit_script.js
+++ assets/js/commit_script.js
@@ -2,71 +2,84 @@ let startButton = document.querySelector("#start");
 let timerElement = document.querySelector("#time");
 let startScreen = document.querySelector("#start-screen");
 let questionScreen = document.querySelector("#questions");
-// let questionTitle = document.querySelector("#question-title");
-// let options = document.querySelector("#choices");
-// let currentscore = document.querySelector("#currScore");
-// let result = document.querySelector("#res");
-// let submit = document.querySelector("#submit");
-// let initial = document.querySelector("#initials");
-// let finalscore = document.querySelector("#final-score");
+let questionTitle = document.querySelector("#question-title");
+let options = document.querySelector("#choices");
+let currentScore = document.querySelector("#currScore");
+let result = document.querySelector("#res");
+let endScreen = document.querySelector("#end-screen");
+let initialEl = document.querySelector("#initials");
+let finalScore = document.querySelector("#final-score");
+let submitBtn = document.querySelector("#submit");
 
 //--------------------Questions---------------------------
+
 let questions = [
   {
     que: "What is the largest mammal on Earth?",
-    options: ["Elephant", "Blue whale", "Giraffe", "Polar bear"],
-    ans: "Blue whale",
+    opt: ["1. Elephant", "2. Blue whale", "3. Giraffe", "4. Polar bear"],
+    ans: "2. Blue whale",
   },
   {
     que: "Which bird is known for its ability to mimic human speech?",
-    options: ["Eagle", "Parrot", "Penguin", "Owl"],
-    ans: "Parrot",
+    opt: ["1. Eagle", "2. Parrot", "3. Penguin", "4. Owl"],
+    ans: "2. Parrot",
   },
   {
     que: "What is the primary diet of pandas?",
-    opt: ["Meat", "Bamboo", "Fish", "Insects"],
-    ans: "Bamboo",
+    opt: ["1. Meat", "2. Bamboo", "3. Fish", "4. Insects"],
+    ans: "2. Bamboo",
   },
   {
     que: "Which of the following animals is a marsupial?",
-    opt: ["Kangaroo", "Elephant", "Cheetah", "Giraffe"],
-    ans: "Kangaroo",
+    opt: ["1. Kangaroo", "2. Elephant", "3. Cheetah", "4. Giraffe"],
+    ans: "1. Kangaroo",
   },
   {
     que: "What is the only mammal capable of sustained flight?",
-    opt: ["Bat", "Pegasus", "Eagle", "Flying squirrel"],
-    ans: "Bat",
+    opt: ["1. Bat", "2. Pegasus", "3. Eagle", "4. Flying squirrel"],
+    ans: "1. Bat",
   },
   {
     que: "Which snake is known for its ability to spit venom at its prey?",
-    opt: ["Python", "King cobra", "Rattlesnake", "Anaconda"],
-    ans: "Rattlesnake",
+    opt: ["1. Python", "2. King cobra", "3. Rattlesnake", "4. Anaconda"],
+    ans: "3. Rattlesnake",
   },
   {
     que: "What is the largest big cat in the world?",
-    opt: ["Leopard", "Lion", "Tiger", "Cheetah"],
-    ans: "Tiger",
+    opt: ["1. Leopard", "2. Lion", "3. Tiger", "4. Cheetah"],
+    ans: "3. Tiger",
   },
   {
-    ques: "Which ocean creature is capable of regenerating its arms?",
-    opt: ["Dolphin", "Octopus", "Shark", "Sea turtle"],
-    ans: "Octopus",
+    que: "Which ocean creature is capable of regenerating its arms?",
+    opt: ["1. Dolphin", "2. Octopus", "3. Shark", "4. Sea turtle"],
+    ans: "2. Octopus",
   },
   {
     que: "What is the fastest land animal?",
-    opt: ["Elephant", "Cheetah", "Horse", "Gazelle"],
-    ans: "Cheetah",
+    opt: ["1. Elephant", "2.Cheetah", "3. Horse", "4. Gazelle"],
+    ans: "2. Cheetah",
   },
   {
     que: "What is the natural habitat of a penguin?",
-    opt: ["Desert", "Rainforest", "Arctic tundra", "Antarctic ice pack"],
-    ans: "Antarctic ice pack",
+    opt: [
+      "1. Desert",
+      "2. Rainforest",
+      "3. Arctic tundra",
+      "4. Antarctic ice pack",
+    ],
+    ans: "4. Antarctic ice pack",
   },
 ];
 
+//---------------------Highscore----------------------------------------
+let highscore = JSON.parse(localStorage.getItem("highScore"));
+if (highscore == null) {
+  highscore = [];
+}
+console.log(highscore);
 //---------------------Timer & start quiz click------------------------------//
 
-let timerOnDisplay = 10;
+let timerOnDisplay = 25;
 let timer;
 
 function startTimer() {
@@ -75,6 +88,7 @@ function startTimer() {
     timerElement.textContent = timerOnDisplay;
     if (timerOnDisplay === 0) {
       clearInterval(timer);
+      showEnd("time up");
     }
   }, 1000);
 }
@@ -86,4 +100,63 @@ function quizStart() {
   startTimer();
   startScreen.setAttribute("class", "hide");
   questionScreen.setAttribute("class", "start");
+
+  showQues();
 }
+
+/*--------------------------Questions and options--------------------------*/
+
+let currQuestion = 0;
+let score = 0;
+
+function showQues() {
+  questionTitle.textContent = questions[currQuestion].que;
+  let prepOpt = "";
+
+  for (let i = 0; i < questions[currQuestion].opt.length; i++) {
+    prepOpt +=
+      '<button class="opt">' + questions[currQuestion].opt[i] + "</button>";
+  }
+
+  choices.innerHTML = prepOpt;
+
+  let optnBtn = document.querySelectorAll(".opt");
+
+  for (let i = 0; i < optnBtn.length; i++) {
+    optnBtn[i].addEventListener("click", function () {
+      if (optnBtn[i].innerHTML == questions[currQuestion].ans) {
+        result.innerHTML = "Right Answer";
+        score += 10;
+      } else {
+        result.innerHTML = "Wrong Answer";
+        score -= 5;
+      }
+      currentScore.innerHTML = score;
+      currQuestion++;
+
+      if (currQuestion < questions.length) {
+        showQues();
+      } else {
+        showEnd();
+      }
+    });
+  }
+}
+
+let showEnd = function (arg) {
+  if (arg == "time up") {
+    let endSc = document.querySelector("#endSc");
+    endSc.innerHTML = "Time up!!";
+  }
+  clearInterval(timer);
+  questionScreen.setAttribute("class", "hide");
+  endScreen.setAttribute("class", "start");
+  finalScore.innerHTML = score;
+  submitBtn.addEventListener("click", () => {
+    let obj = { initial: initialEl.value, score };
+    highscore.push(obj);
+    console.log(highscore);
+    localStorage.setItem("highScore", JSON.stringify(highscore));
+    window.location.href = "highscores.html";
+  });
+};
